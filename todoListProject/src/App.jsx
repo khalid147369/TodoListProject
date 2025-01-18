c19f8600e2a6dc37ad7ba560cbe647f4c2afe9eb
khalid <rabaaouikhalid@gmail.com>
2025-01-18 13:17:22 +0100

    add routing

--- todoListProject/src/App.jsx
+++ todoListProject/src/App.jsx
@@ -1,14 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './Pages/Home';
+import Login from './Pages/Login';
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
- <div>oiyooo</div>
-  )
+    <Router>
+      <div>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login/>} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App
